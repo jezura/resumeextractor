f3c18a5fd664bbcc50dbe053c1d5a6dddb56e19c
Jaroslav Jezek <jaroslav.jezek@uhk.cz>
2019-12-23 12:15:32 +0100

    Initial commit: blank Maven Spring MVC project structure.

---
+++ src/test/java/com/uhk/ppro/firemni_system/FiremniSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhk.ppro.firemni_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiremniSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
